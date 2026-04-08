0825dd3c0c8a1a9ea23fa024087da7e9c39e25cb
miracle boniface <udohmiracle23@gmail.com>
2026-04-08 14:55:41 +0100

    repo updated

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from urllib.parse import quote_plus
+# from urllib.parse import quote_plus
+from dotenv import load_dotenv
+import os
 
-password = quote_plus("Javascript23$")
-db_url = f"postgresql://postgres:{password}@localhost:5432/fast_api_demo"
+load_dotenv()  # loads variables from .env
+
+# password = quote_plus("[password]")
+# # quote_plus is a utility function in the urllib.parse module used for URL-encoding strings, specifically designed for query strings (the part of a URL after the ?). Was used on the password to encode it
+
+db_url = os.getenv("DATABASE_URL") or ''
+
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
